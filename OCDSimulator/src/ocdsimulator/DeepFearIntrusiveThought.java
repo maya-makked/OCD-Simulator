e2a500d1794085bb057b1a9ab4242ddf2fdb545b
maya-makked <98240458+maya-makked@users.noreply.github.com>
2024-05-15 17:36:54 -0400

    Moving obsessionAndCompulsions() to abstract class

--- OCDSimulator/src/ocdsimulator/DeepFearIntrusiveThought.java
+++ OCDSimulator/src/ocdsimulator/DeepFearIntrusiveThought.java
@@ -87,64 +87,7 @@ public class DeepFearIntrusiveThought extends IntrusiveThought {
         String fear = fears[fearNum];
         return fear;
     }
-
-
-    /**
-     * Runs the OCD cycle
-     */
-    @Override
-    public void obsessionAndCompulsions() {
-        // Get obsession
-        int thoughtNum = 0;
-        String intrusiveThought = generateThought(thoughtNum);
-
-        // Compulsions- avoidance, rationalize, reassurance, acceptance
-        // Have to get the proper ones associated with the thought
-        // Get avoidance
-        String avoidance = getAvoidance(0);
-        // Get rationalize
-        String rationalization = getRationalize(0);
-        // Get reassurance
-        String reassurance = getReassurance(0);
-
-        // Loop the options display until user chooses acceptance
-        // Allow quit option so that user can quit
-        boolean cycleBroken = false;
-        while (!cycleBroken) {
-            // Display obsession
-            System.out.println(intrusiveThought + "\n");
-
-            // Display compulsion menu
-            System.out.println("Choose:");
-            System.out.println("1. Avoidance- " + avoidance);
-            System.out.println("2. Rationalization- " + rationalization);
-            System.out.println("3. Reassurance- " + reassurance);
-            System.out.println("4. Acceptance- " + ACCEPTANCE);
-            System.out.println("5. Quit- " + QUIT);
-
-            // Now get user option
-            int option = validateCompulsionInput();
-
-            // Cycle keeps going if options 1-3 picked
-            // Cycle breaks if acceptance chosen
-            if (option == 4) {
-                System.out.println(
-                    "\nThe intrusive thought has peacefully passed by without "
-                        + "bothering you...");
-                cycleBroken = true;
-            }
-            // Quit simulation if quit chosen
-            else if (option == 5) {
-                System.exit(0);
-            }
-
-            System.out.println();
-
-        }
-
-        System.out.println();
-
-    }
+    
 
     /**
      * Gets the avoidance compulsion for the fears
